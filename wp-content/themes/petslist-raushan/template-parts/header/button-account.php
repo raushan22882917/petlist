926f7e8fc9fd7c09a392af14400e104b714d5d13
raushan22882917 <raushan2288.jnvbanka@gmail.com>
2026-06-29 17:46:37 +0530

    Update login destination to /login/ and update SQL database dump

--- wp-content/themes/petslist-raushan/template-parts/header/button-account.php
+++ wp-content/themes/petslist-raushan/template-parts/header/button-account.php
@@ -5,24 +5,27 @@
  * @version 1.0
  */
 
- use RadiusTheme\Petslist\Options;
- use Rtcl\Helpers\Link;
+use RadiusTheme\Petslist\Options;
+use Rtcl\Helpers\Link;
 
-$login_icon_title = is_user_logged_in() ? esc_html__( 'Account', 'petslist' ) : esc_html__( 'Login', 'petslist' );
-$login_link = is_user_logged_in() ? home_url('/dog-dashboard/') : home_url('/dog-login/');
+$login_icon_title = is_user_logged_in() ? esc_html__('Account', 'petslist') : esc_html__('Login', 'petslist');
+$login_link = is_user_logged_in() ? home_url('/dog-dashboard/') : home_url('/login/');
 
 ?>
-    <a class="header-login-icon" data-toggle="tooltip" title="<?php echo esc_html( $login_icon_title ); ?>" href="<?php echo esc_url( $login_link ); ?>">
-        <?php if ( is_user_logged_in() ) : 
-            $user_id = get_current_user_id();
-            $pp_id = absint( get_user_meta( $user_id, '_rtcl_pp_id', true ) );
-            $avatar_url = $pp_id ? wp_get_attachment_image_url( $pp_id, 'thumbnail' ) : get_avatar_url( $user_id, ['size' => 56] );
+<a class="header-login-icon" data-toggle="tooltip" title="<?php echo esc_html($login_icon_title); ?>"
+    href="<?php echo esc_url($login_link); ?>">
+    <?php if (is_user_logged_in()):
+        $user_id = get_current_user_id();
+        $pp_id = absint(get_user_meta($user_id, '_rtcl_pp_id', true));
+        $avatar_url = $pp_id ? wp_get_attachment_image_url($pp_id, 'thumbnail') : get_avatar_url($user_id, ['size' => 56]);
         ?>
-            <span class="header-login-avatar-wrap">
-                <img src="<?php echo esc_url($avatar_url); ?>" alt="<?php echo esc_attr( $login_icon_title ); ?>" class="header-login-avatar" width="32" height="32" style="width:32px;height:32px;border-radius:50%;object-fit:cover;display:block;flex-shrink:0;">
-            </span>
-        <?php else: ?>
-            <i class="icon-pl-account"></i>
-        <?php endif; ?>
-        <span class="header-login-text"><?php echo esc_html( $login_icon_title ); ?></span>
-    </a>
+        <span class="header-login-avatar-wrap">
+            <img src="<?php echo esc_url($avatar_url); ?>" alt="<?php echo esc_attr($login_icon_title); ?>"
+                class="header-login-avatar" width="32" height="32"
+                style="width:32px;height:32px;border-radius:50%;object-fit:cover;display:block;flex-shrink:0;">
+        </span>
+    <?php else: ?>
+        <i class="icon-pl-account"></i>
+    <?php endif; ?>
+    <span class="header-login-text"><?php echo esc_html($login_icon_title); ?></span>
+</a>
