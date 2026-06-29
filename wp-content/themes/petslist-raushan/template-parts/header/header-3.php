3fe21a70d60209135cfbc3c5297a590f3812b72b
raushan22882917 <raushan2288.jnvbanka@gmail.com>
2026-06-29 17:33:04 +0530

    Ensure login/account button is unconditionally loaded in header-3.php

--- wp-content/themes/petslist-raushan/template-parts/header/header-3.php
+++ wp-content/themes/petslist-raushan/template-parts/header/header-3.php
@@ -22,13 +22,11 @@ if ( Options::$options['sticky_header'] == 1 ) {
         <div class="main-header-inner">
             <?php get_template_part( 'template-parts/header/logo', '2' ) ?>
             <?php get_template_part( 'template-parts/header/site', 'nav' ) ?>
-            <?php if ( Helper::is_chat_enabled() || Options::$options['header_login_icon'] || Options::$options['header_btn'] ): ?>
-                <div class="header-icon-area">
-                    <?php get_template_part( 'template-parts/header/button', 'chat' ) ?>
-                    <?php get_template_part( 'template-parts/header/button', 'account' ) ?>
-                    <?php get_template_part( 'template-parts/header/button', 'link' ) ?>
-                </div>
-            <?php endif; ?>
+            <div class="header-icon-area">
+                <?php get_template_part( 'template-parts/header/button', 'chat' ) ?>
+                <?php get_template_part( 'template-parts/header/button', 'account' ) ?>
+                <?php get_template_part( 'template-parts/header/button', 'link' ) ?>
+            </div>
         </div>
     </div>
 </div>
